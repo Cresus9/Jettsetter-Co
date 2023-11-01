374bd12a50349d39722ede5e7b557b172ff0e6d5
Sigui Thierry Herman Yabre <thierry.yabre@yahoo.fr>
2023-11-01 12:01:50 -0400

    added

--- client/src/Components/Rendering/Jet/JetListings.js
+++ client/src/Components/Rendering/Jet/JetListings.js
@@ -5,20 +5,41 @@ import './JetListings.css'
 export default function JetListings({jets, bookme}) {
   const [listings, setListings]= useState([])
 
-useEffect(()=>{
-    fetch (`${process.env.REACT_APP_BACKEND_URL}/jets`)
+// useEffect(()=>{
+//     fetch (`${process.env.REACT_APP_BACKEND_URL}/jets`)
+//     .then((res) => {
+//       if (res.ok) {
+//         res.json().then((listings) => {
+//           setListings(listings);
+//           jets([...listings])
+        
+//         });
+//       } else {
+//         res.json().then((errors) => console.log(errors));
+//       }
+//     })
+// }, []);
+useEffect(() => {
+  fetch(`${process.env.REACT_APP_BACKEND_URL}/jets`)
     .then((res) => {
       if (res.ok) {
-        res.json().then((listings) => {
-          setListings(listings);
-          jets([...listings])
-        
-        });
+        return res.json();
       } else {
-        res.json().then((errors) => console.log(errors));
+        // If the response isn't ok, throw an error to be caught in the catch block
+        return res.json().then((err) => Promise.reject(err));
       }
     })
+    .then((listings) => {
+      setListings(listings);
+      if (typeof setJets === "function") {  // Assuming you have a state updater function named setJets
+        setJets([...listings]);
+      }
+    })
+    .catch((errors) => {
+      console.error(errors);
+    });
 }, []);
+
 
 const getList = listings.map((listing)=>
 <JetCard key={listing.id} jet={listing} bookme={bookme}/>)

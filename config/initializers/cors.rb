076f51389a260a147e8eea23d53453bdb03e5ed8
Sigui Thierry Herman Yabre <thierry.yabre@yahoo.fr>
2023-11-01 11:36:45 -0400

    added

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://jetsetter-coo.onrender.com/'
-
+    origins 'http://localhost:3002'  # replace with your frontend origin
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
   end
 end
+

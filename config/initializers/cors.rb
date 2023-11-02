424cdcde8732e04c99ae91b659c57e9f881f9ecf
Sigui Thierry Herman Yabre <thierry.yabre@yahoo.fr>
2023-11-02 08:34:51 -0400

    added

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,13 +5,13 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'http://localhost:3002'  # replace with your frontend origin
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-#       credentials: true
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'http://localhost:3002'  # replace with your frontend origin
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
+  end
+end
 

467cb08382bbc4ec9ca3a358deb607e5850cdfea
Sigui Thierry Herman Yabre <thierry.yabre@yahoo.fr>
2023-11-02 00:58:50 -0400

    added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
-    before_action :authorized
+    # before_action :authorized
     rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 	rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
  #rescue from JWT::DecodeError, with: :render_unauthorized_response
